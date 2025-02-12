e4e87b0a5ef6f00bf6ce8d6e35e178a1894a5e5a
peafield <peterjohncoles@gmail.com>
2025-02-12 16:25:41 +0000

    Update lefthook to use bunx instead of npx

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import testController from "../controllers/test.controller";
 
+// TODO: DELETE THIS FILE
+
 const router: Router = Router();
 
 router.route("/").get(testController.getTest);
